52ae6b9b71c021598cccd910c77ec9e3ced3c249
Jithsungh Sai Vallabhaneni (TM) <jithsungh.v@techmojo.in>
2025-09-16 15:21:14 +0530

    Update bot configuration: restore default APP_ID and APP_PASSWORD values

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = int(os.environ.get("PORT", 8000))
-    APP_ID = os.environ.get("MicrosoftAppId", "1a0d5572-e3f6-421d-a9e4-068cfc0d421e")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "X4U8Q~a0_xnkoe2Wu7yVb_oHgcF.mR93M3vAuaO_")
+    APP_ID = os.environ.get("MicrosoftAppId", "6d9ae75d-ddd2-479c-8845-5ee08e4a6391")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "AWw8Q~SM6arIUHRAwdPYMuEYxnW8U8OfHJkC7cLE")
     APP_TYPE = os.environ.get("MicrosoftAppType", "SingleTenant")
     APP_TENANT_ID = os.environ.get("MicrosoftAppTenantId", "6f1ac041-698d-4fc7-984b-4a040764d7a2")
