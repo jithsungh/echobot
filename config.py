7bfd4aaee705777c430b72176752fc2bcc454868
Jithsungh Sai Vallabhaneni (TM) <jithsungh.v@techmojo.in>
2025-09-16 12:36:26 +0530

    enhanced debugging

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = int(os.environ.get("PORT", 8000))
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MicrosoftAppId", "1a0d5572-e3f6-421d-a9e4-068cfc0d421e")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "X4U8Q~a0_xnkoe2Wu7yVb_oHgcF.mR93M3vAuaO_")
     APP_TYPE = os.environ.get("MicrosoftAppType", "SingleTenant")
-    APP_TENANT_ID = os.environ.get("MicrosoftAppTenantId", "")
+    APP_TENANT_ID = os.environ.get("MicrosoftAppTenantId", "6f1ac041-698d-4fc7-984b-4a040764d7a2")
